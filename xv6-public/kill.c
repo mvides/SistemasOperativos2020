7fce0eb100c80441b13764cac2b9b0b17475744e
mvides <a01632891@itesm.mx>
2020-09-02 01:39:26 +0000

    practica 2

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -12,6 +12,10 @@ main(int argc, char **argv)
     exit();
   }
   for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+    if (atoi(argv[i]) == 1) {
+      printf(2, "Ni mi mitis\n");
+    } else {
+      kill(atoi(argv[i]));
+    }
   exit();
 }
